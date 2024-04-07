f33a4c63791b02e58ebfacc9197794be095d40ae
sho037 <103867061+sho037@users.noreply.github.com>
2024-04-07 08:24:45 +0000

    学科用テーブル作成

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,5 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_07_082039) do
+  create_table "subjects", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_subjects_on_name", unique: true
+  end
+
 end
