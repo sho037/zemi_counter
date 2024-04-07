ce99a259ab2ef66f96606e6e76a31ed4bcde936d
sho037 <103867061+sho037@users.noreply.github.com>
2024-04-07 11:03:33 +0000

    Webスクレイピング実装

---
+++ api/app/services/web_scraping.rb
@@ -0,0 +1,36 @@
+require "mechanize"
+
+class WebScraping
+  def check
+    agent = Mechanize.new
+    page = agent.get(ENV["SCRAPING_URL"])
+
+    subject = nil
+
+    page.search(".outer tr").each_with_index do |row, index|
+      next if index < 1
+
+      # 学科
+      subject_node = row.at(".head") 
+      if subject_node.present?
+        subject_name = subject_node.text.strip
+        subject = Subject.find_or_create_by(name: subject_name)
+        next
+      end
+
+      # 研究室
+      columns = row.search("td")
+      next if columns.empty?
+      laboratory = Laboratory.find_or_create_by(
+        name: columns[1].text.strip,
+        capacity: columns[2].text.strip,
+        registrants_str: columns[3].text.strip,
+        subject: subject,
+      )
+      # 研究室が登録されている場合は、登録者数を更新する
+      if laboratory.persisted?
+        laboratory.update(registrants_str: columns[3].text.strip)
+      end
+    end
+  end
+end
