ef010fa476ad1f8c485e37f774cfa7b3481ff440
sho037 <103867061+sho037@users.noreply.github.com>
2024-04-07 14:24:28 +0000

    研究室を選択できるように

--- web/src/api/laboratory.ts
+++ web/src/api/laboratory.ts
@@ -1,5 +1,23 @@
 import { get } from "./get";
 
-export const getLaboratories = async () => {
-  return get("/laboratories");
+export type Laboratory = {
+  id: number;
+  name: string;
+  registrants_str: string;
+  subject_id: number;
+};
+
+export const getLaboratories = async ():Promise<Laboratory[]> => {
+  const laboratories: Laboratory[] = await get("/laboratories")
+    .then((response) => {
+      if (!Array.isArray(response.laboratories)) {
+        throw new Error("Response data is not an array");
+      }
+      return response.laboratories;
+    })
+    .catch((error) => {
+      throw new Error(error);
+    });
+
+  return laboratories;
 };
