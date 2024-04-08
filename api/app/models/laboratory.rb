f79a38dec9d67bf3450f9fe4176d41890a7fa798
sho037 <103867061+sho037@users.noreply.github.com>
2024-04-08 08:58:42 +0000

    条件を修正

--- api/app/models/laboratory.rb
+++ api/app/models/laboratory.rb
@@ -1,6 +1,6 @@
 class Laboratory < ApplicationRecord
   belongs_to :subject
   validates :name, presence: true
-  validates :name, uniqueness: true
+  validates :name, uniqueness: { scope: :subject_id }
   validates :capacity, presence: true
 end
